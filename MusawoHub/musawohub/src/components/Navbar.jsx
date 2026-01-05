929e3decbf77b53e886156df300111f486c6949e
Arnold Beeka <arnoldbeeka@gmail.com>
2026-01-05 12:20:18 +0300

    'Started on Musawohub'

---
+++ MusawoHub/musawohub/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { assets } from "../assets/assets";
+
+export default function Navbar(){
+
+    return(
+        <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+            <img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+            <ul className="hidden md:flex items-start gap-5 font-medium">
+                <NavLink to='/'>
+                    <li className="py-1">Home</li>
+                    <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden"/>
+                </NavLink>
+                <NavLink to='/doctors'>
+                    <li className="py-1">All Doctors</li>
+                    <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden"/>
+                </NavLink>
+                <NavLink to='/about'>
+                    <li className="py-1">About</li>
+                    <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden"/>
+                </NavLink>
+                <NavLink to='/contact'>
+                    <li className="py-1">Contact</li>
+                    <hr className="border-none outline-none h-0.5 bg-[#5f6FFF] w-3/5 m-auto hidden"/>
+                </NavLink>
+            </ul>
+            <div>
+                <button className="bg-[#5f6FFF] text-white px-8 py-3">Create Account</button>
+            </div>
+        </div>
+    )
+}
